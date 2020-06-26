19568d18b01c11e2df64286a26ae97c70da7c6ac
Vincent Herlemont <vincentherl@leszeros.com>
2020-06-26 17:45:36 +0000

    sam deploy and create bucket

--- run.sh
+++ run.sh
@@ -1,6 +1,19 @@
 #!/bin/bash
 declare -A all && eval all=($ALL)
-declare -r setup_name=$PROJECT_NAME
 
-declare -p all
-declare -p setup_name
+stack_name=$SHORT_ENV-$SHORT_SETUP
+
+stack_name=$(echo $stack_name | sed s/_/-/g )
+echo stack_name $stack_name
+
+bucket_name=${stack_name}-bucket-deploy
+echo bucket_name $bucket_name
+
+if ! aws s3api head-bucket --bucket "$bucket_name" 2>/dev/null; then
+  aws s3 mb s3://$bucket_name --region $SAM_S3_BUCKET_DEPLOY_REGION
+fi
+
+sam deploy --stack-name $stack_name \
+           --s3-bucket $bucket_name \
+           --region $STACK_REGION \
+           --capabilities CAPABILITY_IAM CAPABILITY_NAMED_IAM
