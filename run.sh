d12475c5df623880af7e07ed3c5f5305a1deb95b
Vincent Herlemont <vincentherl@leszeros.com>
2020-06-27 15:53:45 +0000

    add delete action

--- run.sh
+++ run.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -e
 declare -A all && eval all=($ALL)
 
 stack_name=$SHORT_ENV-$SHORT_SETUP
@@ -9,11 +10,24 @@ echo stack_name $stack_name
 bucket_name=${stack_name}-bucket-deploy
 echo bucket_name $bucket_name
 
-if ! aws s3api head-bucket --bucket "$bucket_name" 2>/dev/null; then
-  aws s3 mb s3://$bucket_name --region $SAM_S3_BUCKET_DEPLOY_REGION
-fi
+if [ "$1" = "delete" ]; then
 
-sam deploy --stack-name $stack_name \
-           --s3-bucket $bucket_name \
-           --region $STACK_REGION \
-           --capabilities CAPABILITY_IAM CAPABILITY_NAMED_IAM
+  aws cloudformation delete-stack --region $STACK_REGION \
+    --stack-name $stack_name
+  aws cloudformation wait stack-delete-complete --region $STACK_REGION \
+    --stack-name $stack_name
+
+  aws s3 rb s3://$bucket_name --force
+
+else
+
+  if ! aws s3api head-bucket --bucket "$bucket_name" 2>/dev/null; then
+    aws s3 mb s3://$bucket_name --region $SAM_S3_BUCKET_DEPLOY_REGION
+  fi
+
+  sam deploy --stack-name $stack_name \
+             --s3-bucket $bucket_name \
+             --region $STACK_REGION \
+             --capabilities CAPABILITY_IAM CAPABILITY_NAMED_IAM
+
+fi
